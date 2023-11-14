dbfc5dde5c5f433a03edaa3342353db349f1295f
Hilmiwi <wijaksanahilmi@gmail.com>
2023-11-14 23:11:12 +0700

    Initial setup client

--- projects/client/src/index.js
+++ projects/client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { theme } from "./themes/theme.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
---
+++ projects/client/src/themes/theme.js
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        // bg: "",
+      },
+    },
+  },
+  colors: {
+    // primaryColor: ,
+    // secondaryColor: ,
+    // thirdColor: ,
+    // forthColor: ,
+    // negativeColor: ,
+    // blackColor: ,
+  },
+  fonts: {
+    // body: ``,
+    // heading: ``,
+  },
+});
