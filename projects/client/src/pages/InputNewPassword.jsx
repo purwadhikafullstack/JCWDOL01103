ae5412be9ec28c4cbc6645106dfe30fbb4779e12
hilmi <wijaksanahilmi@gmail.com>
2024-01-02 00:10:37 +0700

    Fix not found page

--- projects/client/src/pages/InputNewPassword.jsx
+++ projects/client/src/pages/InputNewPassword.jsx
@@ -22,14 +22,16 @@ import { toastConfig } from "../utils/toastConfig";
 import { checkResetToken, getUser, patchNewPassword } from "../api/auth";
 import { useNavigate, useParams } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
+import NotFound from "./NotFound";
 
 function InputNewPassword() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPwd, setShowConfirmPwd] = useState(false);
   const [isLaptop] = useMediaQuery("(min-width: 768px)");
+  const [isLinkValid, setIsLinkValid] = useState(true);
   const toast = useToast();
-  const param = useParams()
-  const navigate = useNavigate()
+  const param = useParams();
+  const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
       newPassword: "",
@@ -48,127 +50,143 @@ function InputNewPassword() {
       try {
         const data = {
           newPassword: values.newPassword,
-          confirmNewPassword: values.confirmNewPassword
-        }
-        const encodedToken = encodeURIComponent(param.token)
-        const response = await patchNewPassword(encodedToken, data)
+          confirmNewPassword: values.confirmNewPassword,
+        };
+        const encodedToken = encodeURIComponent(param.token);
+        const response = await patchNewPassword(encodedToken, data);
         toast(toastConfig("success", "Success", response.data.message));
       } catch (error) {
         toast(toastConfig("error", "Failed", error.response.data.message));
       }
     },
   });
-  useEffect(()=>{
-    (async()=>{
-      const token = jwtDecode(param.token)
+  useEffect(() => {
+    (async () => {
+      const token = jwtDecode(param.token);
       try {
-        const checkToken = await checkResetToken(param.token)
-        if(checkToken.status === 200){
-          const response = await getUser(token.id)
-          response.data.status !== "reset" && navigate("/notfound")
-        }else{
-          return navigate("/notfound")
+        const checkToken = await checkResetToken(param.token);
+        if (checkToken.status === 200) {
+          const response = await getUser(token.id);
+          // response.data.status !== "reset" && navigate("/notfound")
+          response.data.status !== "reset" && setIsLinkValid(false);
+        } else {
+          // return navigate("/notfound")
+          return setIsLinkValid(false);
         }
       } catch (error) {
-        navigate("/notfound")
+        setIsLinkValid(false);
+        // navigate("/notfound")
       }
-    })()
-  },[])
+    })();
+  }, []);
   return (
-    <Center h="100vh" bg="gray.100" flexDir="column" rowGap="1rem">
-      <Flex
-        w="full"
-        flexDir="column"
-        maxW="500px"
-        h="max-content"
-        minH={isLaptop ? "unset" : "full"}
-        borderRadius="2xl"
-        shadow="lg"
-        bg="white"
-        p="10"
-        justify="center"
-      >
-        <Heading mb="5">Change your password</Heading>
-        <form onSubmit={formik.handleSubmit}>
-          <Flex w="full" flexDir="column" rowGap="1rem" justifyContent="center">
-            <VStack alignItems="flex-start">
-              <FormControl
-                isInvalid={
-                  formik.errors.newPassword && formik.touched.newPassword
-                }
+    <>
+      {isLinkValid ? (
+        <Center h="100vh" bg="gray.100" flexDir="column" rowGap="1rem">
+          <Flex
+            w="full"
+            flexDir="column"
+            maxW="500px"
+            h="max-content"
+            minH={isLaptop ? "unset" : "full"}
+            borderRadius="2xl"
+            shadow="lg"
+            bg="white"
+            p="10"
+            justify="center"
+          >
+            <Heading mb="5">Change your password</Heading>
+            <form onSubmit={formik.handleSubmit}>
+              <Flex
+                w="full"
+                flexDir="column"
+                rowGap="1rem"
+                justifyContent="center"
               >
-                <Text>New Password</Text>
-                <InputGroup size="md">
-                  <Input
-                    pr="4.5rem"
-                    id="newPassword"
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Enter new password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.newPassword}
-                    autoComplete="off"
-                  />
-                  <InputRightElement width="30px" mr="2">
-                    <Box
-                      cursor="pointer"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? (
-                        <FaEyeSlash color="grey" />
-                      ) : (
-                        <FaEye color="grey" />
-                      )}
-                    </Box>
-                  </InputRightElement>
-                </InputGroup>
-                <FormErrorMessage>{formik.errors.newPassword}</FormErrorMessage>
-              </FormControl>
-            </VStack>
-            <VStack alignItems="flex-start">
-              <FormControl
-                isInvalid={
-                  formik.errors.confirmNewPassword &&
-                  formik.touched.confirmNewPassword
-                }
-              >
-                <Text>Confirm New Password</Text>
-                <InputGroup size="md">
-                  <Input
-                    id="confirmNewPassword"
-                    pr="4.5rem"
-                    type={showConfirmPwd ? "text" : "password"}
-                    placeholder="Enter confirm new password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.confirmNewPassword}
-                    autoComplete="off"
-                  />
-                  <InputRightElement width="30px" mr="2">
-                    <Box
-                      cursor="pointer"
-                      onClick={() => setShowConfirmPwd(!showConfirmPwd)}
-                    >
-                      {showConfirmPwd ? (
-                        <FaEyeSlash color="grey" />
-                      ) : (
-                        <FaEye color="grey" />
-                      )}
-                    </Box>
-                  </InputRightElement>
-                </InputGroup>
-                <FormErrorMessage>
-                  {formik.errors.confirmNewPassword}
-                </FormErrorMessage>
-              </FormControl>
-            </VStack>
-            <Button type="submit" mt="1rem" bg='primaryColor' color="white">
-              Submit
-            </Button>
+                <VStack alignItems="flex-start">
+                  <FormControl
+                    isInvalid={
+                      formik.errors.newPassword && formik.touched.newPassword
+                    }
+                  >
+                    <Text>New Password</Text>
+                    <InputGroup size="md">
+                      <Input
+                        pr="4.5rem"
+                        id="newPassword"
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter new password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.newPassword}
+                        autoComplete="off"
+                      />
+                      <InputRightElement width="30px" mr="2">
+                        <Box
+                          cursor="pointer"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? (
+                            <FaEyeSlash color="grey" />
+                          ) : (
+                            <FaEye color="grey" />
+                          )}
+                        </Box>
+                      </InputRightElement>
+                    </InputGroup>
+                    <FormErrorMessage>
+                      {formik.errors.newPassword}
+                    </FormErrorMessage>
+                  </FormControl>
+                </VStack>
+                <VStack alignItems="flex-start">
+                  <FormControl
+                    isInvalid={
+                      formik.errors.confirmNewPassword &&
+                      formik.touched.confirmNewPassword
+                    }
+                  >
+                    <Text>Confirm New Password</Text>
+                    <InputGroup size="md">
+                      <Input
+                        id="confirmNewPassword"
+                        pr="4.5rem"
+                        type={showConfirmPwd ? "text" : "password"}
+                        placeholder="Enter confirm new password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.confirmNewPassword}
+                        autoComplete="off"
+                      />
+                      <InputRightElement width="30px" mr="2">
+                        <Box
+                          cursor="pointer"
+                          onClick={() => setShowConfirmPwd(!showConfirmPwd)}
+                        >
+                          {showConfirmPwd ? (
+                            <FaEyeSlash color="grey" />
+                          ) : (
+                            <FaEye color="grey" />
+                          )}
+                        </Box>
+                      </InputRightElement>
+                    </InputGroup>
+                    <FormErrorMessage>
+                      {formik.errors.confirmNewPassword}
+                    </FormErrorMessage>
+                  </FormControl>
+                </VStack>
+                <Button type="submit" mt="1rem" bg="primaryColor" color="white">
+                  Submit
+                </Button>
+              </Flex>
+            </form>
           </Flex>
-        </form>
-      </Flex>
-    </Center>
+        </Center>
+      ) : (
+        <NotFound />
+      )}
+    </>
   );
 }
 
